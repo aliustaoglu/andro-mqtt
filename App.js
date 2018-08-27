238b6555952c39c05dda361d1233ea06906c838b
Cuneyt Aliustaoglu <aliustaoglu@yahoo.com>
2018-08-27 22:27:03 +1200

    Native module added

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import Index from './src/index';
+import { NativeModules } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    NativeModules.Device.getDeviceName((err, name) => console.log(err, name));
+
     return <Index />;
   }
 }
